e7b2560ec1fa456ac813ecbf48818aba94dbf95c
Radisa Jovicevic <radisa@gmail.com>
2022-01-07 19:45:29 +0100

    refactor testing

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,32 +2,36 @@
 
 set -u
 
+cd "$(dirname $0)"
+
 RED='\033[0;31m' # red collor
 GREEN='\033[0;32m' # green collor
 NC='\033[0m' # no Color
-DOCKER_COMPOSE="docker-compose -f docker-compose.yml"
 
-# Prepare test environment
-cd ..
-${DOCKER_COMPOSE} build
-${DOCKER_COMPOSE} up -d
-DOCKER_HOST_URL="http://$(${DOCKER_COMPOSE} port apache 80)"
 
-# Run tests
-cd tests
-for T in $(find . -type f -name 'test_*.sh')
-do
-    echo -n ">>> Running test $T ... "
-    source $T
-    if [ "$?" == 0 ]
-    then
-        echo -e "${GREEN}PASS${NC}"
-    else
-        echo -e "${RED}FAIL${NC}"
-    fi
+for TESTDIR in $(ls -d test*); do
+    cd $TESTDIR
+    cp -f ../../Dockerfile .
+    cp -f ../../000-default.conf .
+    cp -f ../../docker-entrypoint.sh .
+    cp -f ../../Dockerfile .
+    cp -f ../../servername.conf .
+
+    docker-compose build &>/dev/null
+    docker-compose up -d &>/dev/null
+    DOCKER_HOST_URL="http://$(docker-compose port apache 80)"
+    for TESTFILE in $(ls -1 test_*.sh); do
+        echo -n ">>> ${TESTDIR}/${TESTFILE} ... "
+        source ${TESTFILE}
+        if [ "$?" == 0 ]
+        then
+            echo -e "${GREEN}PASS${NC}"
+        else
+            echo -e "${RED}FAIL${NC}"
+        fi
+    done
+    # # Clean test environment
+    docker-compose stop &>/dev/null
+    docker-compose down &>/dev/null
+    cd ..
 done
-
-# Clean test environment
-cd ..
-${DOCKER_COMPOSE} stop
-${DOCKER_COMPOSE} down
